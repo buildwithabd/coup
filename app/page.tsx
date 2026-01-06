70c879cea41416f8f26b3653ab9b9e32b1525aa4
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-06 01:09:04 +0100

    initialize coup app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#007AFF] bg-[linear-gradient(rgba(255,255,255,0.9),rgba(255,255,255,0.9))] bg-blend-normal">
+      <div></div>
+    </div>
+  );
+}
