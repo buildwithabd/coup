1d6de13bea038060067ee54b47e5663f0c69a36c
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-06 01:36:40 +0100

    Add fluid ellipse styles and integrate into homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#007AFF] bg-[linear-gradient(rgba(255,255,255,0.9),rgba(255,255,255,0.9))] bg-blend-normal">
-      <div></div>
+    <div className="relative flex min-h-screen items-center justify-center bg-[#007AFF] bg-[linear-gradient(rgba(255,255,255,0.9),rgba(255,255,255,0.9))] bg-blend-normal">
+      <div className="ellipse"></div>
     </div>
   );
 }
