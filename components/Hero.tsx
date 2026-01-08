865a8627125e91af65e6b500c4c5a776710d60d4
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-09 00:03:38 +0100

    Add Hero component; implement structure and styling for the hero section with ranking information

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="rank">
+          <div className="flex justify-center items-center bg-[#007AFF] w-8 h-[17px]">
+            ...
+          </div>
+          <p>#1 iMessage Automation Tool</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
