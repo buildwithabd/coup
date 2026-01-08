2b4ece436fd8d108d4293bcfc68785eb507316a0
AfricanBudu <abdullahagboola202@gmail.com>
2026-01-08 23:15:15 +0100

    Refactor Navbar component styles; replace inline styles with class-based styling for navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,13 +15,9 @@ function Navbar() {
         />
       </div>
 
-      <div className="relative flex items-center justify-center gap-4 text-base font-medium text-[#8C97A8]">
+      <div className="navbar-links">
         {["How it Works", "Pricing", "Use Case", "FAQ"].map((link) => (
-          <Link
-            key={link}
-            className="py-[8px] px-[12px] hover:text-black transition-colors duration-200"
-            href="/"
-          >
+          <Link key={link} className="" href="/">
             {link}
           </Link>
         ))}
